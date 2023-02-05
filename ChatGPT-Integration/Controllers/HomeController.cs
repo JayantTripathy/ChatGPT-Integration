961c867067e7c7c5f6d15f7e32ec7f92c3b0a44d
jayant <jayanttripathy1985@gmail.com>
2023-02-05 21:48:23 +0530

    Add project files.

---
+++ ChatGPT-Integration/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using ChatGPT_Integration.Models;
+using Microsoft.AspNetCore.Mvc;
+using OpenAI_API.Completions;
+using OpenAI_API;
+using System.Diagnostics;
+
+namespace ChatGPT_Integration.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(string searchText)
+        {
+            //your OpenAI API key
+            string apiKey = "sk-6S92ywcXXXXXXXXXXXXXXXXXXXX";
+            string answer = string.Empty;
+            var openai = new OpenAIAPI(apiKey);
+            CompletionRequest completion = new CompletionRequest();
+            completion.Prompt = searchText;
+            completion.Model = OpenAI_API.Models.Model.DavinciText;
+            completion.MaxTokens = 4000;
+            var result = openai.Completions.CreateCompletionAsync(completion);
+            if (result != null)
+            {
+                foreach (var item in result.Result.Completions)
+                {
+                    answer = item.Text;
+                }
+            }
+            ViewBag.Answer = answer;
+            ViewBag.Text = searchText;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ChatGPT-Integration/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ChatGPT_Integration.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
